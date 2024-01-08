6263d3fbbd8e746211c1201a0bed7536b11715b4
SrinivasBallari <sps21042003@gmail.com>
2024-01-08 20:41:04 +0530

    added: error handler classes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,17 @@
 const UserService = require('../services/userService');
 const userService = new UserService();
 
-const create = async (req, res) => { 
+const create = async (req, res) => {
     try {
         const user = await userService.create(req.body);
-        res.status(201).json({
+        return res.status(201).json({
             message : "success",
             data : user,
             err: {}
         });
     } catch (error) {
         console.log("Error in user-controller");
-        res.status(500).json({
+        return res.status(500).json({
             message : "failed",
             data : {},
             err: error
@@ -22,14 +22,14 @@ const create = async (req, res) => {
 const destroy = async (req, res) => {
     try {
         const response = await userService.destroy(req.query.id);
-        res.status(201).json({
+        return res.status(201).json({
             message : "success",
             data : response,
             err: {}
         });
     } catch (error) {
         console.log("Error in user-controller");
-        res.status(500).json({
+        return res.status(500).json({
             message : "failed",
             data : {},
             err: error
@@ -40,14 +40,14 @@ const destroy = async (req, res) => {
 const signIn = async (req, res) => {
     try {
         const response = await userService.signIn(req.body.email, req.body.password);
-        res.status(200).json({
+        return res.status(200).json({
             message: "successfully signed-in",
             data: response,
             err: {}
         });
     } catch (error) {
         console.log("Error in user-controller");
-        res.status(500).json({
+        return res.status(500).json({
             message : "failed",
             data : {},
             err: error
